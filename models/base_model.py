94f886aa4393deda5dbb825252ea0802fa7cc0de
Davies Aniefiok <daviesaniefiok32@gmail.com>
2023-02-12 04:09:12 -0800

    updated base_model

--- models/base_model.py
+++ models/base_model.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-from datetime import datetime
-import uuid
-import models
 """
 This module contains a class BaseModel that defines\
         all common attributes/methods for other classes:
 """
+import uuid
+from models import storage
+from datetime import datetime
 
 
 class BaseModel():
     """
-    This class contains three public instance attributes\
+    This class contains three public instance attributes
             id | created_at | updated_at
-    This class also contains two publis instance methods\
+    This class also contains two publis instance methods
             save | to_dict
     """
     def __init__(self, *args, **kwargs):
@@ -20,21 +20,21 @@ class BaseModel():
         this initialization method creates a unique id\
                 and creates a time instance
         """
-        if bool(kwargs) is False:
-            self.id = uuid.uuid4()
-            val = datetime.now()
-            self.created_at = val
-            self.updated_at = val
-            models.storage.new(self)
-        else:
-            for key, value in kwargs.items():
-                if key in ["__class__"]:
-                    pass
-                elif key in ["created_at", "updated_at"]:
-                    value = datetime.fromisoformat(value)
-                    self.__setattr__(key, value)
+        if kwargs:
+            for key in kwargs:
+                if key == 'created_at':
+                    self.__dict__['created_at'] = datetime.strptime(
+                            kwargs['created_at'], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == 'updated_at':
+                    self.__dict__['updated_at'] = datetime.strptime(
+                            kwargs['updated_at'], "%Y-%m-%dT%H:%M:%S.%f")
                 else:
-                    self.__setattr__(key, value)
+                    self.__dict__[key] = kwargs[key]
+        else:
+            self.id = uuid.uuid4()
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """
@@ -49,7 +49,7 @@ class BaseModel():
                 updated_at with the current datetime
         """
         self.updated_at = datetime.now()
-        models.storage.save()
+        storage.save()
 
     def to_dict(self):
         """
