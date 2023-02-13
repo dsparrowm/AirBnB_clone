2ec6dc1ceaf997f47ca53184432716eff051bbd0
Davies Aniefiok <daviesaniefiok32@gmail.com>
2023-02-12 17:57:10 -0800

    updates for city

--- models/city.py
+++ models/city.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-"""
-This module contains a single class City inheriting from BaseModel
-"""
+"""This module creates a User class"""
+
 from models.base_model import BaseModel
 
 
 class City(BaseModel):
-    """
-    this contains the representation of the class
-    """
+    """Class for managing city objects"""
+
     state_id = ""
     name = ""
